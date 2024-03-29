2e43475c7b3a30c367ad3f146cb862473f8ece0d
Harrison Souchereau <harrison.souchereau@yale.edu>
2024-03-29 14:32:11 -0400

    Added Akerman2023 built-in

--- galarp/builtins/satellites.py
+++ galarp/builtins/satellites.py
@@ -13,3 +13,11 @@ def JZ2023_Satellite():
 
     return dm + stars + gas
 
+
+def NA2023_Satellite():
+
+
+
+    dm = gp.BurkertPotential(rho=5.93e-25 * u.g / u.cm**3, r0 = 17.36 * u.kpc, units=galactic)
+    stars = gp.MiyamotoNagaiPotential(1e11 * u.Msun, a= 5.94 * u.kpc, b = 0.58 * u.kpc, units=galactic)
+    gas = gp.MiyamotoNagaiPotential(1e10 * u.Msun, a= 10.1 * u.kpc, b = 0.87 * u.kpc, units=galactic)
