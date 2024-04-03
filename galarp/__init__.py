85a7bc2fbde48cbb059c511fa0e4dd31a08d50e1
Harrison Souchereau <harrison.souchereau@yale.edu>
2024-04-03 10:38:43 -0400

    Moved density classes to their own file

--- galarp/__init__.py
+++ galarp/__init__.py
@@ -1,9 +1,10 @@
-from .particle_grids import *  # ParticleGrid class
-from .rampressure import *  # RPS classes and functions
-from .shadows import *  # Shadow classes and functions
-from .tests import *  # Testing utilities
-from .utils import *  # Utility and plotting functions
-from .winds import *  # Classes for ram pressure wind
+from .densities import *            # Density classes
+from .particle_grids import *       # ParticleGrid class
+from .rampressure import *          # RPS classes and functions
+from .shadows import *              # Shadow classes and functions
+from .tests import *                # Testing utilities
+from .utils import *                # Utility and plotting functions
+from .winds import *                # Classes for ram pressure wind
 
 from . import builtins
 
