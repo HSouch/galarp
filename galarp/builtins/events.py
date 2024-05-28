ee9fe8a8de14ea231e5db67f113b53513566c007
Harrison Souchereau <harrison.souchereau@yale.edu>
2024-05-28 18:42:59 -0400

    Fixes to some builtins

--- galarp/builtins/events.py
+++ galarp/builtins/events.py
@@ -52,14 +52,15 @@ def ExampleLorentzianEvent(inc=45, strength=600):
 
     pot = builtins.JZ2023_Satellite()
     mass_profile = gen_mass_profile(pot)
-    wind = winds.LorentzianWind(t0=500 * u.Myr, width = 500 * u.Myr, units=galactic)
-    wind.init_from_inc(inclination=np.deg2rad(inc), strength= strength *u.km/u.s)
+    wind = winds.LorentzianWind()
     
     shadow = shadows.UniformShadow()
     shadow.init_from_wind(wind)
     
     particles = particles.UniformGrid(Rmax= 10, n_particles= 20, z_start= 0.5)
-    particles.generate(mass_profile)
+
+    pset = particles.ParticleSet(particles=particles.PlaneDistribution())
+    pset.generate(mass_profile)
 
     sim = rampressure.RPSim(wind=wind, potential=pot, shadow=shadow)
     orbits = sim.run(particles, rho_icm=1e-26 * u.g/ u.cm**3, integration_time=1000 * u.Myr, dt=2*u.Myr)
