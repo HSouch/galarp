42080a6c620b91267ce9d8dd766712c360d84193
Harrison Souchereau <harrison.souchereau@yale.edu>
2024-06-28 14:24:52 -0400

    Started base for particle tagging

--- galarp/postprocessing/analysis/__init__.py
+++ galarp/postprocessing/analysis/__init__.py
@@ -1,1 +1,2 @@
 from .disk_analysis import *        # Methods for analyzing the disk over an integrated RP event.
+from .tagging import *              # Methods for tagging particles in the simulation.
---
+++ galarp/postprocessing/analysis/tagging.py
@@ -0,0 +1,9 @@
+
+class ParticleTagger:
+    def __init__(self, orbits, args=None, **kwargs):
+        self.orbits = orbits
+        self.args = args
+    
+    def tag(self):
+        pass
+    
